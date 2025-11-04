a4f9fbe94180f96a6292d190076620be410c5f71
Diana Deregi <deregidia@gmail.com>
2025-11-04 22:58:09 +0100

    Added modal to select moods

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -1,13 +1,20 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Checkbox } from 'expo-checkbox';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { addEntry } from '../store/diary';
 
 export default function ModalScreen() {
   const [text, setText] = useState('')
-  const [moods, setMoods] = useState('')
+  const [moods, setMoods] = useState([
+    { isSet: false, mood: 'happy'},
+    { isSet: false, mood: 'sad'},
+    { isSet: false, mood: 'joyful'},
+    { isSet: false, mood: 'depressive'},
+  ])
+  const [moodModalVisible, setMoodModalVisible] = useState(false)
   const now = new Date();
   const router = useRouter()
   const dispatch = useDispatch()
@@ -16,25 +23,66 @@ export default function ModalScreen() {
     dispatch(addEntry({
       id: Date.now().toString(),
       text,
-      moods: moods.split(',').map(mood => mood.trim()),
+      moods: moods.filter(mood => mood.isSet).map(mood => mood.mood),
       created: new Date()
     }))
     router.back()
   }
 
+  const onSetMood = (changedMood: string, isSet: boolean) => {
+    setMoods(prevMoods => prevMoods.map(mood => mood.mood === changedMood ? { mood: mood.mood, isSet } : mood))
+  }
+
+  const onMoodsEdit = () => {
+    setMoodModalVisible(true)
+  }
+
+  const getMoodList = () => {
+    const selectedMoods = moods.filter(mood => mood.isSet).map(mood => mood.mood)
+
+    return selectedMoods.length ? selectedMoods.join(', ') : null
+  }
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={moodModalVisible}
+        onRequestClose={() => {
+          setMoodModalVisible(false);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{gap: 10, paddingBottom: 20}}>
+              {moods.map(mood => (
+              <View style={{ flexDirection: 'row', gap: 5 }}>
+                <Checkbox value={mood.isSet} onValueChange={(value: boolean) => onSetMood(mood.mood, value)}></Checkbox>
+                <Text>{mood.mood}</Text>
+              </View>
+            ))}
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setMoodModalVisible(false)}>
+              <Text>Save</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <View style={{ justifyContent: "space-between", flexDirection: 'row', paddingBottom: 10 }}>
         <Text style={{ color: '#AD8474' }}>{now.toLocaleDateString()}</Text>
         <Text style={{ color: '#AD8474' }}>{now.toLocaleDateString('EN', { weekday: 'long' }) }</Text>
       </View>
-      <TextInput
-          editable
-          onChangeText={value => setMoods(value)}
-          value={moods}
-          style={styles.moodsInput}
-          placeholder='What moods do you feel now?'
-      />
+      <TouchableHighlight onPress={() => setMoodModalVisible(true)} style={styles.moodsInput}>
+        <>
+          <View>
+            <Text>What moods do you feel now?</Text>
+            <Text>{getMoodList()}</Text>
+          </View>
+          <MaterialIcons size={20} name="edit" color='#000'/>
+        </>
+      </TouchableHighlight>
       <TextInput
           editable
           multiline
@@ -73,10 +121,37 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     backgroundColor: '#fff',
     borderRadius: 20,
-    paddingLeft: 20,
-    paddingRight: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   button: {
-
-  }
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 });
